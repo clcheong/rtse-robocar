a14acb785ee59a62a7ae5d49fc056cf005db2a3f
clcheong <clcheong00@gmail.com>
2023-06-09 14:23:09 +0800

    stable draft

---
+++ prog.bat
@@ -0,0 +1,1 @@
+C:\arduino-1.8.19\hardware\tools\avr\bin\avrdude -C C:\arduino-1.8.19\hardware\tools\avr\etc\avrdude.conf -c arduino -b 115200 -P com7 -p atmega328p -U flash:w:.\default\robocarChatGPT.hex
--- robocarChatGPT.c
+++ robocarChatGPT.c
@@ -4,6 +4,20 @@
  *   Updated : 1/5/2023 Modified to show proximity & light sensor usage
  */
 
+ /*
+ Line Sensor Values:
+
+ 000 = 0 = off track
+ 001 = 1 = right sensor on track
+ 010 = 2 = exactly on track
+ 011 = 3 = middle and right on track
+ 100 = 4 = left sensor on track
+ 101 = 5 = left & right sensor on track, but not middle sensor (Not Possible)
+ 110 = 6 = right and middle sensor on track
+ 111 = 7 = all sensors on track
+ 
+ */
+
 #include "..\inc\kernel.h"                  /* Always include these to use uCOS-II      */
 #include "..\inc\hal_robo.h"                /*   and RoboKar HAL                        */
 
@@ -25,13 +39,40 @@ struct robostate
     int rspeed;                             /* right motor speed  (-100 -- +100)        */
     int lspeed;                             /* leftt motor speed  (-100 -- +100)        */
     char obstacle;                          /* obstacle? 1 = yes, 0 = no                */
+
+    // PID Properties
+    double KP;                              /* Adjustment value of P value of PID  */
+    double KD;                              /* Adjustment value of D value of PID */
+    double KI;                           /* Adjustment value of I of PID */
+    double lastError;                       /* last error from Goal */
+    double integral;                     /* integral value of PID */
+    int goal;                               /* goal is to be on the center of the line (010 or 2)*/
+    int prevLeftSpeed;
+    int prevRightSpeed;
+    int inRoundabout;
+    // int prevSense;
+    // int prev2Sense;
 } myrobot;
+
+int constrain(int value, int lowerBound, int upperBound) {
+    if (value < lowerBound) {
+        return lowerBound;
+    }
+    else if (value > upperBound) {
+        return upperBound;
+    }
+    else {
+        return value;
+    }
+}
+
 
 /*------High pririority task----------*/
 void CheckCollision (void *data)
 {
     for(;;)
     {
+        // myrobot.timer++;
         if ( (robo_proxSensor() == 1) )             /* obstacle?                         */
             myrobot.obstacle = 1;                   /* signal obstacle present           */
         else
@@ -51,7 +92,7 @@ void CntrlMotors (void *data)
         speed_r = myrobot.rspeed;
         speed_l = myrobot.lspeed;
         robo_motorSpeed(speed_l, speed_r);
-        OSTimeDlyHMSM(0, 0, 0, 250);                /* Task period ~ 250 ms              */
+        OSTimeDlyHMSM(0, 0, 0, 10);                /* Task period ~ 250 ms              */
     }
 }
 
@@ -63,23 +104,127 @@ void Navig (void *data)
 {
     for (;;)
     {
-        if (myrobot.obstacle == 1)                  /* If blocked then reverse              */
-        {
-            myrobot.rspeed   = -LOW_SPEED;          /* REVERSE */
-            myrobot.lspeed   = -LOW_SPEED;
-        }
-        else                                        /* obstacle is far away & no collision   */
-        {
-            myrobot.rspeed   = MEDIUM_SPEED;        /* move forward with medium speed        */
-            myrobot.lspeed   = MEDIUM_SPEED;
+        // if (myrobot.obstacle == 1)                  /* If blocked then reverse              */
+        // {
+        //     myrobot.rspeed   = -LOW_SPEED;          /* REVERSE */
+        //     myrobot.lspeed   = -LOW_SPEED;
+        // }
+        // else                                        /* obstacle is far away & no collision   */
+        // {
+        //     myrobot.rspeed   = MEDIUM_SPEED;        /* move forward with medium speed        */
+        //     myrobot.lspeed   = MEDIUM_SPEED;
+        // }
+
+		// if (robo_lightSensor() > 80)                /* it is too bright, I'm photophobia     */
+		// {
+		// 	myrobot.rspeed   = -LOW_SPEED;          /* turn right to avoid                   */
+        //     myrobot.lspeed   =  LOW_SPEED;
+		// }
+
+
+        // int position = robo_lineSensor();
+
+        // if(myrobot.timer == 250) {
+        //     robo_Honk();
+        //     myrobot.KP = 0.06;
+        // }
+
+        // if(myrobot.timer == 350) {
+        //     robo_Honk();
+        //     myrobot.KP = 0.03;
+        // }
+
+        if (myrobot.obstacle == 1) {
+            myrobot.lspeed = STOP_SPEED;
+            myrobot.rspeed = STOP_SPEED;
+        } else {
+
+
+            int sense = myrobot.goal;
+
+            int sensorValue = robo_lineSensor();
+
+            if (myrobot.inRoundabout == 1) {
+                // myrobot.lspeed = LOW_SPEED;
+                // myrobot.rspeed = LOW_SPEED;
+                myrobot.KP = 1.2;
+            } else {
+                myrobot.KP = 0.03;
+            }
+
+            if(sensorValue == 0) {
+                // myrobot.lspeed = - 50 - (myrobot.integral * myrobot.KI * 20);
+                // myrobot.rspeed = - 50 + (myrobot.integral * myrobot.KI * 20);
+                // if(myrobot.prev2Sense == 2 && myrobot.prevSense == 7) {
+                //     robo_Honk();
+                //     robo_LED_toggle();
+                //     myrobot.inRoundabout = 1;
+                // }
+
+                myrobot.lspeed = - (myrobot.prevRightSpeed * 1.1);
+                myrobot.rspeed = - (myrobot.prevLeftSpeed * 1.1);
+
+
+                myrobot.KI = 0.0008; // 0.0008
+            } else {
+                // myrobot.KP = 0.045; 
+
+                switch (sensorValue) {
+                    case 2: sense = 1000;
+                            break;
+                    
+                    case 1: sense = 2000;
+                            break;
+
+                    case 3: sense = 1500;
+                            break;
+
+                    case 4: sense = 0;
+                            break;
+
+                    case 5: sense = 1000;
+                            break;
+
+                    case 6: sense = 500;
+                            break;
+                    
+                    case 7: sense = 1000;
+                            break;
+
+                    default: sense = 1000;
+                            break;
+                }
+
+
+                // Calculate error
+
+                int error = myrobot.goal - sense;
+
+                myrobot.integral = myrobot.integral + error;
+
+
+                // Calculate motor adjustments
+                int adjustment = (myrobot.KP * error) + (myrobot.KI * (myrobot.integral)) + (myrobot.KD * (error - myrobot.lastError));
+                // int adjustment = (myrobot.KP * error) + (myrobot.KD * (error - myrobot.lastError));
+
+                // Store error for the next iteration
+                myrobot.lastError = error;
+
+                // Adjust motors
+                myrobot.lspeed = constrain((60 - adjustment), -100 , 100);
+                myrobot.rspeed = constrain((60 + adjustment), -100 , 100);
+                
+
+                myrobot.prevLeftSpeed = myrobot.lspeed;
+                myrobot.prevRightSpeed = myrobot.rspeed;
+                // myrobot.prev2Sense = myrobot.prevSense;
+                // myrobot.prevSense = sensorValue;
+            }
+
+            OSTimeDlyHMSM(0, 0, 0, 10);                /* Task period ~ 500 ms                  */
         }
 
-		if (robo_lightSensor() > 80)                /* it is too bright, I'm photophobia     */
-		{
-			myrobot.rspeed   = -LOW_SPEED;          /* turn right to avoid                   */
-            myrobot.lspeed   =  LOW_SPEED;
-		}
-        OSTimeDlyHMSM(0, 0, 0, 500);                /* Task period ~ 500 ms                  */
+        
     }
 }
 
@@ -108,7 +253,7 @@ void TaskStart( void *data )
     while(1)
     {
         OSTimeDlyHMSM(0, 0, 5, 0);                          /* Task period ~ 5 secs          */
-        robo_LED_toggle();                                  /* Show that we are alive        */
+       // robo_LED_toggle();                                  /* Show that we are alive        */
     }
 
 }
@@ -122,6 +267,19 @@ int main( void )
     myrobot.rspeed   = STOP_SPEED;                         /* Initialize myrobot states      */
     myrobot.lspeed   = STOP_SPEED;
     myrobot.obstacle = 0;                                  /*  No collisioin                 */
+    // myrobot.flag = false;
+    myrobot.KP = 0.03; //0,03
+    myrobot.KD = 0.4; //0.07
+    myrobot.KI = 0.0005; //0.0004
+    myrobot.lastError = 0.0;
+    myrobot.integral = 0.0;
+    // myrobot.timer = 0;
+    myrobot.prevLeftSpeed = 0;
+    myrobot.prevRightSpeed = 0;
+    myrobot.inRoundabout = 0;
+    // myrobot.prevSense = 0;
+    // myrobot.prev2Sense = 0;
+    myrobot.goal = 1000;                                      /* goal is to follow on the middle sensor */
 
     OSTaskCreate(TaskStart,                                /* create TaskStart Task          */
                 (void *)0,
@@ -131,4 +289,3 @@ int main( void )
     OSStart();                                             /* Start multitasking             */
     while (1);                                             /* die here                       */
 }
-
